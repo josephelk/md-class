03ba21ee9e4972efadbb5a3ea694cfde0a2e1ceb
josephelk <162306823+josephelk@users.noreply.github.com>
2025-03-17 14:13:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 
 def main():
-  st.title('Dermatolog Machine Learning')
+  st.title('Dermatology Machine Learning')
   st.info('This App using Machine Learning')
 
 if __name__== "__main__":

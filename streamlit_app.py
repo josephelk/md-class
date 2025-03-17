1a1f5675506bb7fb507fe1eca8f98d351d74954e
josephelk <162306823+josephelk@users.noreply.github.com>
2025-03-17 14:16:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import joblib
 def main():
   st.title('Dermatology Machine Learning')
   st.info('This App using Machine Learning')
-
+  
+  #input data by user
+  erythema = st.slider('Erythema', min_value=0, max_value=3, value=2)
+  
 if __name__== "__main__":
   main()
